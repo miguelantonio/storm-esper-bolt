5195331993804395e243965e2145aea69560299c
Miguel Fuentes Buchholtz <contacto@migue.cl>
2015-01-24 02:00:50 -0300

    License, read and POM

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # storm-esper-bolt
-NO
 
-slightly based on storm-esper from tomdz
+Library that integrates [Esper](http://esper.codehaus.org) as a [Storm](https://github.com/nathanmarz/storm) Bolt.
+
+
+## License
+
+[The Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.txt).
+
+
+## Description
+
+This library defines a Storm Bolt that makes possible to execute Esper EPL and SODA queries on Storm streams:
+
+    TopologyBuilder builder = new TopologyBuilder();
+    builder.setSpout("quotes", new RandomSentenceSpout());
+
+	//Defining input events for Esper
+    Map<String, Object> eventTypes = new HashMap<>();
+	    eventTypes.put("symbol", String.class);
+	    eventTypes.put("price", Integer.class);
+
+	//Using the Bolt
+    builder.setBolt("esper", (new EsperBolt())
+            .addEventTypes(eventTypes)
+            .addOutputTypes(Collections.singletonMap("Result", Arrays.asList("avg", "price")))
+            .addStatements(Collections.singleton("insert into Result "
+                            + "select avg(price) as avg, price from "
+                            + "quotes_default(symbol='A').win:length(2) "
+                            + "having avg(price) > 60.0")))
+            .shuffleGrouping("quotes");
+    builder.setBolt("print", new PrinterBolt()).shuffleGrouping("esper", "Result");
+
+I recommend seeing the Test for the SODA query.
--- src/main/java/com/variacode/cep/storm/esper/EsperBolt.java
+++ src/main/java/com/variacode/cep/storm/esper/EsperBolt.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Variacode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.variacode.cep.storm.esper;
 
 import backtype.storm.generated.GlobalStreamId;
@@ -211,13 +226,20 @@ public class EsperBolt extends BaseRichBolt implements UpdateListener {
             return;
         }
         for (EventBean newEvent : newEvents) {
+            List<Object> tuple = new ArrayList<>(outputTypes.get(newEvent.getEventType().getName()).size());
+            String eventType;
             if (outputTypes.containsKey(newEvent.getEventType().getName())) {
-                List<Object> tuple = new ArrayList<>(outputTypes.get(newEvent.getEventType().getName()).size());
+                eventType = newEvent.getEventType().getName();
                 for (String field : outputTypes.get(newEvent.getEventType().getName())) {
                     tuple.add(newEvent.get(field));
                 }
-                collector.emit(newEvent.getEventType().getName(), tuple);
+            } else {
+                eventType = "default";
+                for (String field : newEvent.getEventType().getPropertyNames()) {
+                    tuple.add(newEvent.get(field));
+                }
             }
+            collector.emit(eventType, tuple);
         }
     }
 
